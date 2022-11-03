190929c4ec196c9297cb74a5331875c30ca94ed5
CodeByO <code.by.o831@gmail.com>
2022-11-04 05:05:35 +0900

    success Injection using pyinjector

--- auto_dll.py
+++ auto_dll.py
@@ -1,11 +1,18 @@
 import os
 import time
 from tqdm import tqdm
+from pyinjector import inject
+import subprocess
 def list_dll(pid):
     stream = os.popen("Listdlls.exe " + str(pid))
     dlls = stream.read()
-    program_name = dlls.split("\n")[7]
-    program_name = program_name.replace("Command line: ", '').replace('"', '').replace(' -bystartup','').replace('\\','/')
+    program_name = ""
+    try:
+        program_name = dlls.split("\n")[7]
+        program_name = program_name.replace("Command line: ", '').replace('"', '').replace(' -bystartup','').replace('\\','/')
+    except:
+        print("Wrong pid...")
+    
     
     if 'C:/' not in program_name:
         name_split = program_name.split('/')
@@ -60,14 +67,20 @@ def find_string(name,list_name,list_dir):
         else:
             return False
     
-
+def create_process(path):
+        return subprocess.Popen([path]).pid
 
 if __name__=='__main__':
-    program_name, list_dlls = list_dll(30096)
+    path_exe = "C:/Users/codeb/Desktop/ExamDLL2/CreateDLL/x64/Debug/MainDLL.exe"
+    path_dll = "C:/Users/codeb/Desktop/CreateDLL.dll"
+    pid = create_process(path_exe)
+    
+    program_name, list_dlls = list_dll(pid)
     file_list,dir_list = check_permission(list_dlls)
     if not len(file_list) == 0:
         if find_string(program_name,file_list,dir_list):
             print("Detect programs vulnerable to dll injection")
+            inject(pid,path_dll)
         else:
             print("Nah....")
     else:
