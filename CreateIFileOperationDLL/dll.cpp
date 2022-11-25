a7eea1b7ffad42c1af57d642cad7cd972f767699
CodeByO <code.by.o831@gmail.com>
2022-11-25 22:34:14 +0900

    add cmake dll creater

---
+++ CreateIFileOperationDLL/dll.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+#include <iostream>
+#include <shobjidl.h>
+
+using namespace std;
+
+void hello() {
+    AllocConsole();
+    freopen("CONOUT$", "w", stdout);
+    cout << "Hello, World!" << endl;
+}
+
+bool __stdcall DllMain(HMODULE /*module*/, DWORD reason, LPVOID /*reserved*/) {
+    if (reason == DLL_PROCESS_ATTACH) hello();
+    return true;
+}
