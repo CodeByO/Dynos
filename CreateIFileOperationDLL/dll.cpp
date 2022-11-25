67ba80990ba69cbf523269e5f9ba3f1ade649ebf
CodeByO <code.by.o831@gmail.com>
2022-11-25 22:50:08 +0900

    Add IFileOperation

--- CreateIFileOperationDLL/dll.cpp
+++ CreateIFileOperationDLL/dll.cpp
@@ -10,6 +10,91 @@ void hello() {
     cout << "Hello, World!" << endl;
 }
 
+HRESULT CopyItem(__in PCWSTR pszSrcItem, __in PCWSTR pszDest, PCWSTR pszNewName)
+{
+    //
+    // Initialize COM as STA.
+    //
+    HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE); 
+    if (SUCCEEDED(hr))
+    {
+        IFileOperation *pfo;
+  
+        //
+        // Create the IFileOperation interface 
+        //
+        hr = CoCreateInstance(CLSID_FileOperation, 
+                              NULL, 
+                              CLSCTX_ALL, 
+                              IID_PPV_ARGS(&pfo));
+        if (SUCCEEDED(hr))
+        {
+            //
+            // Set the operation flags. Turn off all UI from being shown to the
+            // user during the operation. This includes error, confirmation,
+            // and progress dialogs.
+            //
+            hr = pfo->SetOperationFlags(FOF_NO_UI);
+            if (SUCCEEDED(hr))
+            {
+                //
+                // Create an IShellItem from the supplied source path.
+                //
+                IShellItem *psiFrom = NULL;
+                hr = SHCreateItemFromParsingName(pszSrcItem, 
+                                                 NULL, 
+                                                 IID_PPV_ARGS(&psiFrom));
+                if (SUCCEEDED(hr))
+                {
+                    IShellItem *psiTo = NULL;
+  
+                    if (NULL != pszDest)
+                    {
+                        //
+                        // Create an IShellItem from the supplied 
+                        // destination path.
+                        //
+                        hr = SHCreateItemFromParsingName(pszDest, 
+                                                         NULL, 
+                                                         IID_PPV_ARGS(&psiTo));
+                    }
+                    
+                    if (SUCCEEDED(hr))
+                    {
+                        //
+                        // Add the operation
+                        //
+                        hr = pfo->CopyItem(psiFrom, psiTo, pszNewName, NULL);
+
+                        if (NULL != psiTo)
+                        {
+                            psiTo->Release();
+                        }
+                    }
+                    
+                    psiFrom->Release();
+                }
+                
+                if (SUCCEEDED(hr))
+                {
+                    //
+                    // Perform the operation to copy the file.
+                    //
+                    hr = pfo->PerformOperations();
+                }        
+            }
+            
+            //
+            // Release the IFileOperation interface.
+            //
+            pfo->Release();
+        }
+  
+        CoUninitialize();
+    }
+    return hr;
+}
+
 bool __stdcall DllMain(HMODULE /*module*/, DWORD reason, LPVOID /*reserved*/) {
     if (reason == DLL_PROCESS_ATTACH) hello();
     return true;
