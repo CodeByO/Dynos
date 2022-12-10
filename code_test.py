e768d813e08f3199d46032d4c20559953c565a87
CodeByO <code.by.o831@gmail.com>
2022-12-10 21:40:34 +0900

    update code_test.py

--- code_test.py
+++ code_test.py
@@ -2,12 +2,12 @@ import unittest
 import os
 from pyinjector import inject
 import psutil
-from pywinauto.application import Application
-import pywinauto
-import time
-import subprocess
+
+
 import Dynos
 import warnings
+import subprocess
+from uac_bypass import uac_bypass
 '''
 테스트 목록
 1. Listdlls가 정상 동작하는지
@@ -20,17 +20,46 @@ import warnings
 
 5. pyinjector가 정상동작 하는지
 '''
+def create_process(path):
+    return subprocess.Popen([path]).pid
 
+def list_dll(pid):
+    
+        list_dll_path = os.getcwd() + "/" + "Listdlls.exe "
+        list_dll_path = list_dll_path.replace("\\","/")
+        stream = os.popen(list_dll_path + str(pid))
+        
+        dlls = stream.read()
+        program_name = ""
+        list_dlls = []
+        list_programs = []
+        path_lists = []
+
+        try:
+            split_text = dlls.replace("\\","/").replace('  ','\n').split('\n')
+            
+       
+            for i in split_text:
+                path_lists.append(i.replace(" ",""))
+            
+            for i in path_lists:
+                if i.endswith(".exe"):
+                    list_programs.append(i)
+                if i.endswith(".dll"):
+                    list_dlls.append(i)   
+            for i in list_programs:
+                if i.startswith("C:/"):
+                    program_name = i
+        except:
+            print("\nERROR::rWrong pid...")
+        return program_name, list_dlls
 class DynosTest(unittest.TestCase):
 
 
     def setUp(self):
         warnings.filterwarnings(action='ignore')
-
-        self.hijack = Dynos.hiJacking()
-        self.inject = Dynos.injection()
-        self.uac_bypass = Dynos.uac_bypass()
         
+        self.uac_bypass = uac_bypass()
         self.mainDll_path = os.getcwd() + "/ExamDLL2/CreateDLL/x64/Debug/MainDLL.exe"
         self.mainDll_path = self.mainDll_path.replace("\\","/")
           
@@ -40,9 +69,9 @@ class DynosTest(unittest.TestCase):
         self.assertTrue(cmake.startswith("Usage"))
     
     def test_listdll(self):
-        pid = self.hijack.create_process(self.mainDll_path)
+        pid = create_process(self.mainDll_path)
         
-        programName, self.list_dlls = self.hijack.list_dll(pid)
+        programName, self.list_dlls = list_dll(pid)
         self.assertIsInstance(programName,str)
         self.assertIsInstance(self.list_dlls,list)
         
@@ -63,7 +92,7 @@ class DynosTest(unittest.TestCase):
         psutil.Process(pid).kill()  
 
     def test_injection(self):
-        pid = self.hijack.create_process(self.mainDll_path)
+        pid = create_process(self.mainDll_path)
         dll_path = os.getcwd() + "/test.dll"
         self.assertIsInstance(inject(pid,dll_path),int)
         
