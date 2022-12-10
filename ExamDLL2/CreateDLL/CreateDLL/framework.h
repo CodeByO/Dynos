b803644def295b8a15b8b6a5c500b72c3722534a
CodeByO <code.by.o831@gmail.com>
2022-12-10 18:16:23 +0900

    merge user input branch(not done)

---
+++ ExamDLL2/CreateDLL/CreateDLL/framework.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+// Windows 헤더 파일
+#include <windows.h>
+#include <iostream>
+#include <objbase.h>
+#include <ShobjIdl.h>
+#include <shobjidl_core.h>
+#include <Shellapi.h>     // Included for shell constants such as FO_* values
+#include <shlobj.h>       // Required for necessary shell dependencies
+#include <strsafe.h> 
+#include <string>
---
+++ ExamDLL2/CreateDLL/MainDLL/MainDLL.cpp
@@ -0,0 +1,30 @@
+﻿// MainDLL.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <Windows.h>
+#include <iostream>
+#include <stdlib.h>
+//#include "../CreateDLL/MyMath.h"
+
+typedef double (*DLL_SUM)(double a, double b);
+
+int main()
+{
+	std::string path = "CreateDLL.dll";
+
+	HMODULE hDll = ::LoadLibraryA(path.c_str()); Sleep(1000);
+	if (hDll != NULL)
+	{
+		DLL_SUM pFunc = (DLL_SUM)::GetProcAddress(hDll, "Sum");
+		double ret = pFunc(5.0, 7.0);
+		std::cout << "Result: " << ret << std::endl;
+
+		::FreeLibrary(hDll);
+		system("pause");
+	}
+	else {
+		std::cout << "Fail!" << std::endl;
+	}
+
+}
+
